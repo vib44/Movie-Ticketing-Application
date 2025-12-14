f3cbe98df2b8bc9e0545ea1283390335fd7f9c4b
Vibhuti <singh.vibhuti1997@gmail.com>
2025-12-14 18:26:05 +0530

    Register User to Movie Ticketing Application

---
+++ server/models/user_models.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose")
+
+const userSchema=new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required:true
+        },
+        email:{type: String,
+            required:true,
+            unique: true
+        },
+        password:{
+            type: String,
+            required: true
+        },
+        role:
+        {
+            type: String,
+            enum: ['admin','user','partner'],
+            required: true,
+            default: 'user'
+        }
+         
+    },{timestamp: true})
+
+    const user=mongoose.model('user',userSchema)
+
+    module.exports=user;
